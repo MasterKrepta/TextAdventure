0c1fba1b98e61b4ac2219eed8aab56493df8363f
MasterKrepta <brianj.pajak@gmail.com>
2018-03-15 17:56:43 -0400

    Added new rules for what rooms keys are used in

--- Rules.cs
+++ Rules.cs
@@ -6,12 +6,25 @@ using System.Threading.Tasks;
 
 namespace TextAdventure {
     static class  Rules {
+        // room to bring red key to
         const int redRoomX = 1;
         const int redRoomY = 2;
 
+        // room to bring blue key to
         const int blueRoomX = 2;
         const int blueRoomY = 1;
 
+        // room to bring yellow key to
+        const int yellowRoomX = 3;
+        const int yellowRoomY = 1;
+
+        //room containing password
+        const int passwordRoomX = 3;
+        const int passwordRoomY = 2;
+
+        //Final room to bring password to
+        const int commandRoomX = 0;
+        const int commandRoomY = 1;
         public static bool CanUse(string item) {
             bool result = true; // for testing
             Room room = Game.currentRoom;
@@ -30,6 +43,15 @@ namespace TextAdventure {
                     if (room.LocX == blueRoomX && room.LocY == blueRoomY) {
                         room.AddExit("right");
                         Console.WriteLine("\n    The key unlocks the Blue door");
+                    }
+                    else {
+                        Console.WriteLine("\n     This doenst seem to be the room to use the " + item + " in.");
+                    }
+                    break;
+                case "yellow key":
+                    if (room.LocX == yellowRoomX && room.LocY == yellowRoomY) {
+                        room.AddExit("up");
+                        Console.WriteLine("\n    The key unlocks the Yellow door");
                     }
                     else {
                         Console.WriteLine("\n     This doenst seem to be the room to use the " + item + " in.");
